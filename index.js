704107689ef7f8d1ebc2a2c55704248c99e3f894
harryzhang <harryzhang@bjcjitec.com>
2024-12-04 17:49:06 -0800

    edit index.js

--- index.js
+++ index.js
@@ -1,57 +1,21 @@
-import "dotenv/config";
-import express from 'express';
-import mongoose from "mongoose";
+import express from 'express'
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import UserRoutes from "./Kanbas/Users/routes.js";
 import cors from "cors";
 import "dotenv/config";
-import dotenv from 'dotenv';
 import session from "express-session";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import EnrollmentsRoutes from "./Kanbas/Enrollments/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
-
-
-// Load the correct .env file
-const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
-dotenv.config({ path: envFile });
-
-// connect to the kanbas database
-const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
-mongoose.connect(CONNECTION_STRING);
-const db = mongoose.connection;
-db.once("open", () => {
-  console.log("Database connection is open");
-});
-
 const app = express();
-
-// Dynamic CORS Configuration
-const allowedOrigins = [
-  "http://localhost:3000", // Local development
-];
-
-// Dynamically allow Netlify branch deploy URLs
-if (process.env.NODE_ENV === "production") {
-  allowedOrigins.push(/\.netlify\.app$/); // Match all *.netlify.app subdomains
-}
-
 app.use(
   cors({
     credentials: true,
-    origin: (origin, callback) => {
-      // Allow if no origin (e.g., server-to-server) or origin matches allowedOrigins
-      if (!origin || allowedOrigins.some((o) => (typeof o === "string" ? o === origin : o.test(origin)))) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
   })
 );
-
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kanbas",
   resave: false,
